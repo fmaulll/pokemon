5776fcae96985a4c43de1dc081ddc98780efce18
fmaulll <maul2821@gmail.com>
2023-04-19 23:33:36 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./layout";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
