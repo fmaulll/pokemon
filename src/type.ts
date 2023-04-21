19992a410b41ae726b05cff9d76550e937379354
fmaulll <maul2821@gmail.com>
2023-04-21 17:58:59 +0700

    added new type for bag

--- src/type.ts
+++ src/type.ts
@@ -6,3 +6,12 @@ export type PokemonTypes = {
     height: number,
     types: string
 }
+export type PokemonBagTypes = {
+    id: number,
+    alias: string,
+    name: string,
+    image: string,
+    weight: number,
+    height: number,
+    types: string
+}
