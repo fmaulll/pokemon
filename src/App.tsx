6a915040179d0e1dbcc86a76e736ab0b9aecf6e5
fmaulll <maul2821@gmail.com>
2023-04-21 17:58:16 +0700

    pokemoncontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PokemonProvider from "./context/PokemonContext";
 import Layout from "./layout";
+import Bag from "./pages/Bag";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Layout>
+      <PokemonProvider>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bag" element={<Bag />} />
+          </Routes>
+        </Layout>
+      </PokemonProvider>
     </BrowserRouter>
   );
 }
---
+++ src/context/PokemonContext.tsx
@@ -0,0 +1,61 @@
+import { createContext, FC, useEffect, useState } from "react";
+import { PokemonBagTypes } from "../type";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+interface PokemonContextData {
+  bag: PokemonBagTypes[];
+  loading: boolean;
+  addPokemon: (data: PokemonBagTypes) => void;
+  removePokemon: (id: number) => void;
+  setLoading: (bool: boolean) => void;
+}
+
+const initialValue = {
+  bag: [],
+  loading: false,
+  addPokemon: () => {},
+  removePokemon: () => {},
+  setLoading: () => {},
+};
+
+export const PokemonContext = createContext<PokemonContextData>(initialValue);
+
+const { Provider } = PokemonContext;
+
+const PokemonProvider: FC<Props> = ({ children }) => {
+  const [bag, setBag] = useState<PokemonBagTypes[]>([]);
+  const [loading, setLoadingPokemon] = useState<boolean>(false);
+
+  const setLoading = (bool: boolean) => {
+    setLoadingPokemon(bool);
+  };
+
+  const addPokemon = (data: PokemonBagTypes) => {
+    setBag((prev) => {
+      return [...prev, data];
+    });
+  };
+
+  const removePokemon = (id: number) => {
+    setBag((prev) => {
+      const arr: any = [];
+      prev.map((item) => {
+        if (item.id !== id) {
+          arr.push(item);
+        }
+      });
+      return arr;
+    });
+  };
+
+  return (
+    <Provider value={{ bag, loading, addPokemon, removePokemon, setLoading }}>
+      {children}
+    </Provider>
+  );
+};
+
+export default PokemonProvider;
